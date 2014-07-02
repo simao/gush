49c3f115310734ca120c7e621e69f457496360fe
Simão Mata <simao.mata@wimdu.com>
2014-07-02 15:57:08 +0200

    Refactor Event Listeners

--- src/main/scala/esper/EventStreamListeners.scala
+++ src/main/scala/esper/EventStreamListeners.scala
@@ -19,9 +19,12 @@ class EsperEventListenersManager extends Logging {
 
     val builder = new EventObserverBuilder(epService)
 
-    (new NewBookingEventListener).init(builder)
-    (new BookingAvgNetRevenue(10 seconds)).init(builder)
-    (new BookingCountWindow).init(builder)
+    (new NewBookingCount).init(builder)
+    (new BookingAvgNetRevenue).init(builder)
+    (new BookingsCount).init(builder)
+    (new NewUsersCount).init(builder)
+    (new ReviewsAvg).init(builder)
+    (new ReviewsCount).init(builder)
 
     epService
   }
@@ -53,63 +56,91 @@ class EventObserverBuilder(epService: EPServiceProvider) extends Logging {
   }
 }
 
-abstract class WindowedEvent(val interval: Duration = 5 seconds) {
-  var lastVal = 0.0
-
+abstract class WindowedEvent(val interval: Duration) {
   val intervalSecs = interval.toSeconds
-
-  def colorize(in: Option[Double]) = {
-    in match {
-      case Some(i) => if(i > lastVal)
-        GREEN + i + RESET
-      else if(i < lastVal)
-        RED + i + RESET
-      else
-        YELLOW + i + RESET
-
-      case None => BOLD + "ERROR" + RESET
-    }
-  }
 }
 
-class NewBookingEventListener extends StatsdSender with Logging {
+abstract class WindowedCount(interval: Duration) extends WindowedEvent(interval) with StatsdSender with Logging {
+
+  def table_name: String
+  def statsd_key_name: String
+
   def init(event_builder: EventObserverBuilder) = {
-    val exp = "SELECT * FROM BinlogEsperEvent where tableName='bookings'"
-    event_builder
-      .observer(exp)
-      .map(_ => statsd.increment("gush.booking.total"))
-      .subscribe { _ => logger.info("New booking received") }
-  }
-}
-
-class BookingAvgNetRevenue(interval: Duration = 10 seconds) extends WindowedEvent(interval) with StatsdSender with Logging {
-  def init(event_builder: EventObserverBuilder) = {
-    val exp = s"SELECT avg(asFloat('net_revenue')) as avg_rev FROM BinlogEsperEvent.win:time_batch($intervalSecs second) where tableName='booking_pricing_details'"
-
-    event_builder
-      .observer(exp)
-      .map(e => Option(e.get("avg_rev")).map(x => x.asInstanceOf[Double] / 1000))
-      .subscribe { avg => avg.map(x => {
-          lastVal = x
-          statsd.gauge("gush.booking.avg_revenue_10seconds", x)
-      })
-
-      logger.info(s"Avg net revenue of last ${intervalSecs} seconds: ${colorize(avg)}")
-    }
-  }
-}
-
-class BookingCountWindow(interval: Duration = 10 seconds) extends WindowedEvent(interval) with StatsdSender with Logging {
-  def init(event_builder: EventObserverBuilder) = {
-    val exp = s"SELECT count(*) as count FROM BinlogEsperEvent.win:time_batch($intervalSecs second) where tableName='bookings'"
+    val exp = s"SELECT count(*) as count FROM BinlogEsperEvent.win:time($intervalSecs second) where tableName='${table_name}' OUTPUT LAST EVERY 30 SECONDS"
 
     event_builder
       .observer(exp)
       .map(e => Option(e.get("count")).map(x => x.asInstanceOf[Long].toDouble))
       .subscribe { count =>
-        count.map(x => lastVal = x)
-        count.map(x => statsd.increment("gush.booking.count_10seconds", x.toInt))
-        logger.info(s"Number of bookings in last ${intervalSecs} seconds: ${colorize(count)}")
+        count.map(x => statsd.gauge(statsd_key_name, x.toInt))
+        logger.info(s"Number of ${table_name} in last ${intervalSecs} seconds: ${count}")
     }
   }
+}
+
+abstract class WindowedAvg(interval: Duration) extends WindowedEvent(interval) with StatsdSender with Logging {
+
+  def table_name: String
+  def field_name: String
+  def statsd_key_name: String
+
+  def init(event_builder: EventObserverBuilder) = {
+    val exp = s"SELECT avg(asFloat('${field_name}')) as ${field_name} FROM BinlogEsperEvent.win:time($intervalSecs second) where tableName='${table_name}' OUTPUT LAST EVERY 30 SECONDS"
+
+    event_builder
+      .observer(exp)
+      .map(e => Option(e.get(field_name)).map(x => x.asInstanceOf[Double]))
+      .subscribe { avg => avg.map(x => {
+          statsd.gauge(statsd_key_name, x)
+          logger.info(s"Avg ${field_name} of last ${intervalSecs} seconds: ${avg}")
+      })
+    }
+  }
+
+}
+
+abstract class EventCount extends StatsdSender with Logging {
+  def table_name: String
+  def statsd_key_name: String
+
+  def init(event_builder: EventObserverBuilder) = {
+    val exp = s"SELECT * FROM BinlogEsperEvent where tableName='$table_name'"
+
+    event_builder
+      .observer(exp)
+      .map(_ => statsd.increment(statsd_key_name))
+      .subscribe { _ => logger.info(s"New $table_name received") }
+  }
+}
+
+class NewBookingCount extends EventCount {
+  def table_name = "bookings"
+  def statsd_key_name = "gush.bookings.total"
+}
+
+class BookingAvgNetRevenue extends WindowedAvg(1 hour) {
+  def table_name = "booking_pricing_details"
+  def field_name = "net_revenue"
+  def statsd_key_name = "gush.bookings.net_revenue.avg_1hour"
+}
+
+class BookingsCount extends WindowedCount(1 hour) {
+  def table_name = "bookings"
+  def statsd_key_name = "gush.bookings.count_1hour"
+}
+
+class NewUsersCount extends WindowedCount(1 hour) {
+  def table_name = "users"
+  def statsd_key_name = "gush.users.count_1hour"
+}
+
+class ReviewsAvg extends WindowedAvg(1 hour) {
+  def table_name = "reviews"
+  def field_name = "rating"
+  def statsd_key_name = "gush.reviews.rating.avg_1hour"
+}
+
+class ReviewsCount extends WindowedCount(1 hour) {
+  def table_name = "reviews"
+  def statsd_key_name = "gush.reviews.count_1hour"
 }
