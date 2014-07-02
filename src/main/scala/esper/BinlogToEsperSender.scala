1e9b4064237f6cc0279d5df11f5467ee7dc2e767
Simão Mata <simao.mata@wimdu.com>
2014-07-02 15:56:52 +0200

    Continue even after onError is called

--- src/main/scala/esper/BinlogToEsperSender.scala
+++ src/main/scala/esper/BinlogToEsperSender.scala
@@ -6,20 +6,24 @@ import esper._
 import com.espertech.esper.client.EPServiceProvider
 import rx.lang.scala.Observable
 
+import com.typesafe.scalalogging.log4j._
+
 trait BinlogSqlStream {
   def events: Observable[String]
 }
 
 class BinlogEventStream(eventStream: BinlogSqlStream) {
   def inserts: Observable[BinlogEvent] = {
-    eventStream.events.filter(s => s.startsWith("INSERT INTO"))
+    eventStream.events
+      .filter(s => s.startsWith("INSERT INTO"))
       .filter(s => !s.contains("ON DUPLICATE KEY UPDATE"))
       .map(s => BinlogEvent(s))
   }
 }
 
-class BinlogToEsperSender(cepService: EPServiceProvider, user: String, password: String, host: String, port: Int) {
+class BinlogToEsperSender(cepService: EPServiceProvider, user: String, password: String, host: String, port: Int) extends Logging {
   def sendToEsper(event: BinlogEvent) = {
+    logger.debug(s"Sending event for table ${event.tableName} to ESPER")
     val esperEvent = new BinlogEsperEvent(event.tableName, event.fields)
     cepService.getEPRuntime.sendEvent(esperEvent)
   }
@@ -37,6 +41,16 @@ class BinlogToEsperSender(cepService: EPServiceProvider, user: String, password:
     val stream = remoteStream
     val o = stream.inserts
 
-    o.subscribe(sendToEsper(_))
+    o.onErrorFlatMap({(ex, value) => {
+      value.map(v => logger.error("Error processing: ${v}"))
+      logger.error("Error: ", ex)
+      Observable.empty
+
+      // TODO: No retry...
+      }
+    }).subscribe(
+      { binlog_event => sendToEsper(binlog_event) },
+      {ex => logger.error("onError => ${ex.message}")}
+    )
   }
 }
